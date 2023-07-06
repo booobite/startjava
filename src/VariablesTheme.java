d7864132a61fb4a4641b330e52c4f7e75cad21c8
mariyanishtun <booobite@gmail.com>
2023-07-06 20:58:34 +0300

    homework 0

---
+++ src/VariablesTheme.java
@@ -0,0 +1,37 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class VariablesTheme {
+    public static void main(String[] args) {
+
+        System.out.print("1.Вывод характеристик компьютера");
+
+        byte cheap = 2;
+        short smile = 8;
+        int ok = 8;
+        long os = 84194110;
+        float like = 84194110;
+        double model = 14.2;
+        char tip = 'M';
+        boolean tie = true;
+
+        System.out.println(cheap);
+        System.out.println(smile);
+        System.out.println(ok);
+        System.out.println(os);
+        System.out.println(like);
+        System.out.println(model);
+        System.out.println(tip);
+        System.out.println(tie);
+        System.out.println("'cheap' - это версия чипа");
+        System.out.println("'smile' - это количество ядер");
+        System.out.println("'ok' - это количество памяти");
+        System.out.println("'os' - это версия загрузки ОС");
+        System.out.println("'like' - это версия системной прошивки");
+        System.out.println("'model' - это ижентификатор модели");
+        System.out.println("'tip' - это тип чипа");
+        System.out.println("'tie' - это реальные данные");
+
+
+
+         }
+    }
