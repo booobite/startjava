6356aeb4d9300fac0522efcc9c358f7e90bca526
mariyanishtun <booobite@gmail.com>
2023-07-10 23:34:39 +0300

    add 2 task

--- src/VariablesTheme.java
+++ src/VariablesTheme.java
@@ -1,9 +1,9 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class VariablesTheme {
-    public static void main(String[] args) {
 
-        System.out.print("1.Вывод характеристик компьютера");
+    public static void main(String[] args) {
+        System.out.println("1.Вывод характеристик компьютера");
 
         byte cheap = 2;
         short smile = 8;
@@ -22,14 +22,28 @@ public class VariablesTheme {
         System.out.println(model);
         System.out.println(tip);
         System.out.println(tie);
-        System.out.println("'cheap' - это версия чипа");
-        System.out.println("'smile' - это количество ядер");
-        System.out.println("'ok' - это количество памяти");
-        System.out.println("'os' - это версия загрузки ОС");
-        System.out.println("'like' - это версия системной прошивки");
-        System.out.println("'model' - это идентификатор модели");
-        System.out.println("'tip' - это тип чипа");
-        System.out.println("'tie' - это реальные данные");
+        System.out.println("2 - это версия чипа");
+        System.out.println("8 - это количество ядер");
+        System.out.println("8 - это количество памяти");
+        System.out.println("84194110 - это версия загрузки ОС");
+        System.out.println("84194110 - это версия системной прошивки");
+        System.out.println("14.2 - это идентификатор модели");
+        System.out.println("M - это тип чипа");
+        System.out.println("true - это реальные данные");
+
+
+        System.out.println("2.Расчет стоимости товара со скидкой");
+
+        int pen = 100;
+        int book = 200;
+
+        int generalPrice = pen + book;
+        int discount = 300/100*11;
+        int finishPrice = generalPrice - discount;
+
+        System.out.println(generalPrice);
+        System.out.println(discount);
+        System.out.println(finishPrice);
 
 
 
